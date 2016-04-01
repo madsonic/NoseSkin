7be33e864738564442a7897a8acbd3bf4e1ef1b5
Gerald <ngjingsiang@gmail.com>
2016-04-01 13:51:04 +0800

    final version

--- NoseSkin.ino
+++ NoseSkin.ino
@@ -1,3 +1,6 @@
+// author NG JING SIANG (GERALD)
+// MATRIC NO.: A0124321Y
+
 #include <LiquidCrystal.h>
 #include <MQ135.h>
 
@@ -9,18 +12,21 @@ const int rows = 2;
 const int buttonAnalogPort = A0;
 const int tempAnalogPort = A1;
 const int gasAnalogPort = A2;
+const int ledPort = 13;
 
 int previousBtnState = 0;
 int currentBtnState = 0;
 unsigned long previousTime = 0;
 const int refreshRate = 300;
 const int btnStates[] = {0, 1, 2};
-const int btn0Val = 1020;
-const int btn1Val = 990;
-const int btn2Val = 500;
+const int btn0Val = 1000;
+const int btn1Val = 950;
+const int btn2Val = 490;
+
+float gasThreshold = 400.0;
 
 void setup() {
-  Serial.begin(9600);
+  pinMode(ledPort, OUTPUT);
   lcd.begin(columns, rows);
   lcd.clear();
   lcd.print("Temperature");
@@ -30,7 +36,6 @@ void setup() {
 
 void loop() {
   int btnVal = analogRead(buttonAnalogPort);
-  Serial.println(btnVal);
 
   // checked button press
   // doesnt set when no button pressed
@@ -42,34 +47,59 @@ void loop() {
     currentBtnState = btnStates[2];
   }
 
-  // update screen
+  // // update screen
   unsigned long currentTime = millis();
   if (currentTime - previousTime > refreshRate) {
-    Serial.println(currentTime - previousTime);
-    previousTime = currentTime;
-    previousBtnState = currentBtnState;
-
+    // menu
     if (previousBtnState == 0) {
+      // reset
+      digitalWrite(ledPort, LOW);
       lcd.clear();
+
       lcd.print("Temperature");
       lcd.setCursor(0, 1);
       lcd.print("Gas");
     } else if (previousBtnState == 1) {
-      int tempAnalogVal = analogRead(tempAnalogPort);
-      float tempVolt = (tempAnalogVal / 1024.0) * 5;
-      float temp = (tempVolt - 0.5) * 100;
-      Serial.println(temp);
+      // reset
+      digitalWrite(ledPort, LOW);
 
+      float temp = getTemperature(tempAnalogPort);
+
+      // update temp display value
       lcd.clear();
       lcd.print("Temp: ");
       lcd.print(temp);
       lcd.print(" C");
-      // do lighting also
-    } else if (previousBtnState == 2){
+    } else if (previousBtnState == 2) {
+
+      float gasLevel = gasSensor.getPPM();
+      // update gas display value
       lcd.clear();
       lcd.print("CO2: ");
-      lcd.print(gasSensor.getPPM());
+      lcd.print(gasLevel);
       lcd.print(" ppm");
+
+      // control LED
+      if (gasSensor.getPPM() >= gasThreshold) {
+        digitalWrite(ledPort, HIGH);
+      } else {
+        digitalWrite(ledPort, LOW);
+      }
+
     }
+    // update time
+    previousTime = currentTime;
+    previousBtnState = currentBtnState;
   }
+}
+
+float getTemperature(int pin) {
+  // the numbers are for converting the analog value to temperature
+  float analogRange = 1024.0;
+
+  int tempAnalogVal = analogRead(tempAnalogPort);
+  float tempVolt = (tempAnalogVal / analogRange) * 5;
+  float temp = (tempVolt - 0.5) * 100;
+
+  return temp;
 }
